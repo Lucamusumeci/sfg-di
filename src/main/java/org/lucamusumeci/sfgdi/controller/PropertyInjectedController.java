1404e8db605cb6399398d91a0d4b9ee34f732e0d
Lucamusumeci <lucamusumeci@live.com>
2022-12-07 20:27:52 +0100

    Showing @Primary

--- src/main/java/org/lucamusumeci/sfgdi/controller/PropertyInjectedController.java
+++ src/main/java/org/lucamusumeci/sfgdi/controller/PropertyInjectedController.java
@@ -2,13 +2,11 @@ package org.lucamusumeci.sfgdi.controller;
 
 import org.lucamusumeci.sfgdi.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
-    @Qualifier("greetingServiceImpl")
     @Autowired
     public GreetingService greetingService;
 
--- src/main/java/org/lucamusumeci/sfgdi/service/GreetingServiceImpl.java
+++ src/main/java/org/lucamusumeci/sfgdi/service/GreetingServiceImpl.java
@@ -1,11 +1,13 @@
 package org.lucamusumeci.sfgdi.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+@Primary
 @Service
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayHello() {
-        return "Henlo";
+        return "Greetings! I'm the Primary service.";
     }
 }
