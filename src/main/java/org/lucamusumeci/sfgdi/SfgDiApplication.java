30816127af99ba642940e4a6b47d13e2bfb3724f
Lucamusumeci <lucamusumeci@live.com>
2022-12-07 19:36:32 +0100

    Showing different types on DI done in Spring (and manually)

--- src/main/java/org/lucamusumeci/sfgdi/SfgDiApplication.java
+++ src/main/java/org/lucamusumeci/sfgdi/SfgDiApplication.java
@@ -1,7 +1,6 @@
 package org.lucamusumeci.sfgdi;
 
 import org.lucamusumeci.sfgdi.controller.ConstructorInjectedController;
-import org.lucamusumeci.sfgdi.controller.NonSpringPropertyInjectedController;
 import org.lucamusumeci.sfgdi.controller.PropertyInjectedController;
 import org.lucamusumeci.sfgdi.controller.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
@@ -13,8 +12,6 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
-		NonSpringPropertyInjectedController myController = (NonSpringPropertyInjectedController) context.getBean("myController");
-		System.out.println("Springless: " + myController.sayHello());
 
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) context.getBean("propertyInjectedController");
 		System.out.println("By Property: " + propertyInjectedController.getGreeting());
