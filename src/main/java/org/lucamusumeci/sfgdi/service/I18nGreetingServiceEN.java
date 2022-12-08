43264799baae92de7e53d40e4d6ff47472edc534
Lucamusumeci <lucamusumeci@live.com>
2022-12-08 15:43:48 +0100

    Showing Default Profile

--- src/main/java/org/lucamusumeci/sfgdi/service/I18nGreetingServiceEN.java
+++ src/main/java/org/lucamusumeci/sfgdi/service/I18nGreetingServiceEN.java
@@ -3,7 +3,7 @@ package org.lucamusumeci.sfgdi.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("I18n")
 public class I18nGreetingServiceEN implements GreetingService {
     @Override
